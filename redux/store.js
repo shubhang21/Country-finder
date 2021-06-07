037c3805fbed20786e1ac8f998bbacdaf64fc6ca
shubhang21 <62418957+shubhang21@users.noreply.github.com>
2021-06-07 10:28:54 +0530

    Update store.js

--- redux/store.js
+++ redux/store.js
@@ -3,7 +3,5 @@ import thunk from "redux-thunk";
 import logger from "redux-logger";
 import rootReducer from "./rootReducer";
 
-console.log("Store Created");
-
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 export default store;
